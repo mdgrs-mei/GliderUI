676711ecaad850c973568285d7401e5521da03a4
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-04-12 15:02:09 +0900

    DataSource implements IReflectableType to support binding

--- src/GliderUI.Server/System/DataSource.cs
+++ src/GliderUI.Server/System/DataSource.cs
@@ -1,18 +1,14 @@
 ﻿using System.ComponentModel;
+using System.Reflection;
 
 namespace GliderUI.Server;
 
 #pragma warning disable CA1515 // Consider making public types internal
-public sealed class DataSource : INotifyPropertyChanged
+public sealed class DataSource : INotifyPropertyChanged, IReflectableType
 #pragma warning restore CA1515
 {
     private readonly Dictionary<string, object?> _members = [];
     public event PropertyChangedEventHandler? PropertyChanged;
-
-    internal bool TryGetMember(string memberName, out object? value)
-    {
-        return _members.TryGetValue(memberName, out value);
-    }
 
     public object? GetMember(string memberName)
     {
@@ -23,5 +19,16 @@ public sealed class DataSource : INotifyPropertyChanged
     {
         _members[memberName] = value;
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+    }
+
+    // To make dynamic properties discoverable by Avalonia, implement IReflectableType.
+    public TypeInfo GetTypeInfo()
+    {
+        return new DataSourceTypeInfo(this);
+    }
+
+    internal string[] GetMemberNames()
+    {
+        return [.. _members.Keys];
     }
 }
---
+++ src/GliderUI.Server/System/DataSourcePropertyInfo.cs
@@ -0,0 +1,85 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace GliderUI.Server;
+
+internal sealed class DataSourcePropertyInfo : PropertyInfo
+{
+    private readonly string _name;
+    private readonly Type _propertyType;
+
+    public override PropertyAttributes Attributes { get => PropertyAttributes.None; }
+    public override bool CanRead { get => true; }
+    public override bool CanWrite { get => true; }
+    public override Type PropertyType { get => _propertyType; }
+    public override Type? DeclaringType { get => typeof(DataSource); }
+    public override string Name { get => _name; }
+    public override Type? ReflectedType { get => DeclaringType; }
+
+    public DataSourcePropertyInfo(
+        string name,
+        Type propertyType)
+    {
+        _name = name;
+        _propertyType = propertyType;
+    }
+
+    public override MethodInfo[] GetAccessors(bool nonPublic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object[] GetCustomAttributes(bool inherit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object[] GetCustomAttributes(Type attributeType, bool inherit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override MethodInfo? GetGetMethod(bool nonPublic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ParameterInfo[] GetIndexParameters()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override MethodInfo? GetSetMethod(bool nonPublic)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object? GetValue(object? obj, BindingFlags invokeAttr, Binder? binder, object?[]? index, CultureInfo? culture)
+    {
+        if (obj is DataSource dataSource)
+        {
+            return dataSource.GetMember(Name);
+        }
+        else
+        {
+            throw new ArgumentException($"Object must be of type {typeof(DataSource).FullName}");
+        }
+    }
+
+    public override bool IsDefined(Type attributeType, bool inherit)
+    {
+        return false;
+    }
+
+    public override void SetValue(object? obj, object? value, BindingFlags invokeAttr, Binder? binder, object?[]? index, CultureInfo? culture)
+    {
+        if (obj is DataSource dataSource)
+        {
+            dataSource.SetMember(Name, value);
+        }
+        else
+        {
+            throw new ArgumentException($"Object must be of type {typeof(DataSource).FullName}");
+        }
+    }
+}
