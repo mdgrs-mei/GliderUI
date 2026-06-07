a062c7913142da4fc77e1e0e96b978bbaf1d2f17
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-06-07 21:22:24 +0900

    Add LiveCharts2

--- src/GliderUI.ApiExporter/Program.cs
+++ src/GliderUI.ApiExporter/Program.cs
@@ -76,6 +76,7 @@ internal sealed class Program
         exporter.AddTypesInAssembly(typeof(AvaloniaRuntimeXamlLoader)); // Avalonia.Markup.Xaml.Loader.dll
         exporter.AddTypesInAssembly(typeof(DataGrid)); // Avalonia.Controls.DataGrid.dll
         exporter.AddTypesInAssembly(typeof(NativeWebView)); // Avalonia.Controls.WebView.dll
+        exporter.AddTypesInAssembly(typeof(LiveChartsCore.SkiaSharpView.Avalonia.CartesianChart)); // LiveChartsCore.SkiaSharpView.Avalonia.dll
 
         exporter.AddObject(typeof(Server.DataSourcePropertyComparer));
         exporter.AddTypeMapping(typeof(Server.DataSource));
